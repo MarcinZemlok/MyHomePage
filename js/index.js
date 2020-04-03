18eefacf5c71418ad0e5338667e0d56ed140f152
MarcinZemlok <marcinzemlok@gmail.com>
2020-04-03 15:47:42 +0200

    Circular links and force reload.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+/*//////////////////////////////////////////////////////////////////////////////
+///                                MyHomePage                                ///
+//============================================================================//
+//        Author: Marcin Żemlok
+//         Email: marcinzemlok@gmail.com
+//       Version: 0.2
+//
+//   Description: MyHomePage functionality.
+//
+// Creation date: 20/02/2020
+================================= CHANGE LOG ===================================
+// [20/02/2020]        Marcin Żemlok
+        Added links wheel functionality draft.                               ///
+// [03/04/2020]        Marcin Żemlok
+        * Added force reload button.
+        * Added circular link box.                                           ///
+//////////////////////////////////////////////////////////////////////////////*/
 ////////////////////////////////////////////////////////////////////////////////
 //  DATE & CLOCK FUNCTIONALITY                                              ///
 //////////////////////////////////////////////////////////////////////////////
@@ -38,6 +55,8 @@ window.setInterval(clock, 1000);
 //  INSPIRING TEXT                                                          ///
 //////////////////////////////////////////////////////////////////////////////
 
+const reloadButton = document.querySelector(".controll button");
+
 var mzGetQuery = {
     url: "",
     options: {},
@@ -53,7 +72,7 @@ var mzGetQuery = {
     }
 }
 
-async function getDailyText() {
+async function getDailyText(event=null, force=true) {
     const updateOutput = () => {
         const mainps = document.querySelectorAll(".main p");
         mainps[0].innerHTML = `<em>\"${dailyText.quote}\"</em>`;
@@ -74,14 +93,14 @@ async function getDailyText() {
         });
     });
 
-    if (res.dailyText != undefined) {
+    if (res.dailyText != undefined || force) {
         dailyText.author = res.dailyText.author;
         dailyText.quote = res.dailyText.quote;
         dailyText.background = res.dailyText.background;
     }
 
     const day = Math.floor(Date.now() / 86400000);
-    if (res.dailyText == undefined || day != res.dailyText.update) {
+    if (res.dailyText == undefined || day != res.dailyText.update || force) {
 
         // GET inpireting text
         const quote = await new Promise((resolve) => {
@@ -137,18 +156,72 @@ async function getDailyText() {
     updateOutput();
 }
 
-getDailyText();
+getDailyText(null, false);
+
+reloadButton.addEventListener('click', getDailyText);
 
 ////////////////////////////////////////////////////////////////////////////////
 //  LINKS                                                                   ///
 //////////////////////////////////////////////////////////////////////////////
+class Links {
+    constructor() {
+        this.links = document.querySelector(".links");
 
-document.addEventListener("click", flipLinks);
+        this.items = this.links.querySelectorAll(".link");
 
-function flipLinks() {
-    const links = document.querySelector(".links");
-    if (document.activeElement === document.querySelector(".google-search-form input")) {
-        return;
+        this.shape = 5 * (60 * 2**(1/2));
+
+        this.links.style.height = `${this.shape}px`
+        this.links.style.width = `${this.shape}px`
+
+        this.spaceoutItems();
+
+        document.addEventListener("click", (event) => {
+            this.flip(event);
+        });
     }
-    links.classList.toggle("links-visible");
+
+    spaceoutItems() {
+        const offset = this.shape / 2;
+        let radius = (60 * 2**(1/2));
+        let position = { x: 0, y: -radius };
+        let angleInc = 2 * Math.PI / 8;
+        let angle = 0;
+
+        this.items.forEach((e, i) => {
+            e.style.top = `${position.y + offset}px`;
+            e.style.left = `${position.x + offset}px`;
+
+            const _x = position.x * Math.cos(angleInc) - position.y * Math.sin(angleInc);
+            const _y = position.x * Math.sin(angleInc) + position.y * Math.cos(angleInc);
+
+            position.x = Math.round(_x);
+            position.y = Math.round(_y);
+
+            angle += angleInc;
+
+            if (angle >= Math.PI * 2) {
+                angle = 0;
+                radius = (60 * 2**(1/2)) * 2;
+                position = { x: 0, y: -radius };
+                angleInc = 2 * Math.PI / 17;
+            }
+        });
+    }
+
+    flip(event) {
+
+        if (document.activeElement === document.querySelector(".google-search-form input") ||
+            document.activeElement === document.querySelector(".controll button")) {
+            this.links.classList.remove("links-visible");
+            return;
+        }
+
+        this.links.style.top = `${event.clientY}px`;
+        this.links.style.left = `${event.clientX}px`;
+
+        this.links.classList.toggle("links-visible");
+    }
 }
+
+const links = new Links();
